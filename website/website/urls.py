8c677e4a77042f4a9c91dc210e29e71c96f83022
PKSingh0017 <priyanshusingh1998@gmail.com>
2020-11-11 12:29:12 +0530

    Rendered the templates

--- website/website/urls.py
+++ website/website/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from bookstore import views as bookstoreview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', bookstoreview.home),
+    path('store/', bookstoreview.bookstore)
 ]
