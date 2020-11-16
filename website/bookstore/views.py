ba1a74dcf987d7240bdf4127d9d1d7b516d42789
PKSingh0017 <priyanshusingh1998@gmail.com>
2020-11-16 20:30:40 +0530

    Started from scratch

--- website/bookstore/views.py
+++ website/bookstore/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
-def bookstore(request):
-    return render(request, 'bookstore/bookstore.html')
+books =[
+    {
+        'title': 'Brief answer to the big questions',
+        'author': 'Stefen Hawking',
+        'Highlights': ''
+    }
+]
 
 def home(request):
-    return render(request, 'bookstore/rough.html')
+    return render(request, 'bookstore/home.html')
--- website/website/urls.py
+++ website/website/urls.py
@@ -1,7 +1,7 @@
 """website URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from bookstore import views as bookstoreview
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', bookstoreview.home),
-    path('store/', bookstoreview.bookstore)
+    path('admin/', admin.site.urls, name='bookstore-admin'),
+    path('', include('bookstore.urls')),
 ]
