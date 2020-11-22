aa5aed606ef293ba75363ca3e681a39dff69b828
PKSingh0017 <priyanshusingh1998@gmail.com>
2020-11-22 19:33:15 +0530

    Added the checkout page(not yet final), separated the nav-bar html page, added the order-snippet template

--- website/users/models.py
+++ website/users/models.py
@@ -4,7 +4,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE)
+    stripe_customer_id = models.CharField(max_length=50, blank=True, null=True)
+    one_click_purchasing = models.BooleanField(default=False)
+
 
     def __str__(self):
         return f'{self.user.username} Profile'
